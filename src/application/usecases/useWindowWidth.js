19ee374539cd1c90f0f0b38377432cd53a9827ed
SamuelRBatista <samuka-srb@live.com>
2025-01-13 23:01:35 -0300

    feat:13012025 => hooks para o usecases

---
+++ src/application/usecases/useWindowWidth.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+// Adicione este hook ao seu componente
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowWidth;
+};
