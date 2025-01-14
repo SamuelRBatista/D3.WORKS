9e63796ad9705e69045609acc985915b875d3482
SamuelRBatista <samuka-srb@live.com>
2025-01-13 22:59:47 -0300

    feat:13012025 => entidade do quiz

---
+++ src/domain/entities/Question.js
@@ -0,0 +1,12 @@
+export class Question {
+  constructor(id, title, question, options,correctAnswer, isSelect, isTextEditor) {
+    this.id = id;
+    this.title = title;
+    this.question = question;
+    this.correctAnswer = correctAnswer;
+    this.options = options || [];
+    this.isSelect = isSelect || false;
+    this.isTextEditor = isTextEditor || false;
+    }
+  }
+  
